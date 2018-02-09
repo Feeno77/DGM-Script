742e200841b3692cde07f079c5971d567af5152a
Unknown <alanferons@gmail.com>
2018-02-09 09:54:52 -0700

    Prefabs and singleton

---
+++ Brick Breaker/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public static MusicManager instance = null; //Singleton
+
+    // Use this for initialization
+    void Awake () {
+          if (instance == null) //If instance is not assigned, 
+    {
+        instance = this;        //Assign it to this
+    }
+          else if (instance != this)  //If instance is not this
+        {
+            Destroy(this.gameObject);  //Destroy it
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+}
+	
+
+}
