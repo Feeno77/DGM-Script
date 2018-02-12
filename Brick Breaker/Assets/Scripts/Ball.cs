393d144413e5ea0509f3467cff69905dc3298693
Unknown <alanferons@gmail.com>
2018-02-12 09:55:00 -0700

    Added paddle script

---
+++ Brick Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+
+    [RequireComponent(typeof(AudioSource))]
+    public class ExampleClass : MonoBehaviour
+    {
+        public AudioClip clip;
+        void Start()
+        {
+            AudioSource.PlayClipAtPoint(clip, new Vector3(5, 1, 2));
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Brick Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        //get current paddle position
+        Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
+        //get mouse horizontal position
+        float mousePositionX = Input.mousePosition.x / Screen.width *16 - 8;
+        //save new position
+        paddlePosition.x = mousePositionX;
+        //set paddle object to saved position 
+        this.gameObject.transform.position = paddlePosition;
+	}
+}
